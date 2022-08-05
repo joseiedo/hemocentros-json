2061bce0b6d913479c61d717fabb872e17d71794
jose iedo <iedoduarte1@gmail.com>
2022-08-05 16:12:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const li = document.createElement("li");
+const ul = document.querySelector("ul");
+
+async function iniciar() {
+  // pegando informacoes do arquivo json
+  const hemocentrosResponse = fetch("./hemocentros-db.json");
+  const hemocentrosJSON = await (await hemocentrosResponse).json();
+
+  // criando cards de cada hemocentro e colocando na ul
+  hemocentrosJSON.map((hemocentro) => {
+    const li = document.createElement("li");
+    if (hemocentro.estado) {
+      const estado = hemocentro.estado,
+        nomeHemocentro = hemocentro.nome,
+        endereco = hemocentro.endereco,
+        telefone = hemocentro.tel.join(" "),
+        mapa = `https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=${endereco}+(T%C3%ADtulo)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed`;
+
+      const tituloCard = `<h1>${estado}</h1>`,
+        subtituloCard = `<h2>${nomeHemocentro}</h2>`,
+        enderecoCard = `<p>${endereco}</p>`,
+        contatoCard = `<p>${telefone}</p>`;
+
+      li.innerHTML += `<div>${tituloCard}${subtituloCard}</div>`;
+      li.innerHTML += `<div>${enderecoCard}${contatoCard}</div>`;
+      li.innerHTML += `<iframe src="${mapa}" frameborder="0"></iframe>`;
+
+      ul.appendChild(li);
+    }
+  });
+}
+
+iniciar();
