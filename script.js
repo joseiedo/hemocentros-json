4de25984d745851db2d6ec7bddb9b59b9251c286
jose iedo <iedoduarte1@gmail.com>
2022-08-06 20:16:43 -0300

    css removido do html  e colocado em uma pagina separada, select inserido

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ async function iniciar() {
   // criando cards de cada hemocentro e colocando na ul
   hemocentrosJSON.map((hemocentro) => {
     const li = document.createElement("li");
-    if (hemocentro.estado) {
+    if (hemocentro.estado == "Minas Gerais") {
       const estado = hemocentro.estado,
         nomeHemocentro = hemocentro.nome,
         endereco = hemocentro.endereco,
-        telefone = hemocentro.tel.join("<br>");
-      mapa = `https://www.google.com.br/maps/place/${endereco}`;
-      // mapa = `https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=${endereco}+(T%C3%ADtulo)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed`;
+        telefone = hemocentro.tel.join("<br>"),
+        // mapa = `https://www.google.com.br/maps/place/${endereco}`;
+        mapa = `https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=${endereco}+(T%C3%ADtulo)&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed`;
 
       const tituloCard = `<h1>${estado}</h1>`,
         subtituloCard = `<h2>${nomeHemocentro}</h2>`,
@@ -25,8 +25,8 @@ async function iniciar() {
 
       li.innerHTML += `<div>${tituloCard}${subtituloCard}</div>`;
       li.innerHTML += `<div>${enderecoCard}${contatoCard}</div>`;
-      li.innerHTML += linkMapCard;
-      // li.innerHTML += `<iframe src="${mapa}" frameborder="0"></iframe>`;
+      // li.innerHTML += linkMapCard;
+      li.innerHTML += `<iframe src="${mapa}" frameborder="0"></iframe>`;
 
       ul.appendChild(li);
     }
